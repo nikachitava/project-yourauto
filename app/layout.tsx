ecd664699fe63af0179ad541abe8045341e6706c
nikachitava <chitava18.official@gmail.com>
2025-04-25 22:11:26 +0400

    add subset to font

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const poppins = Poppins({
     variable: "--font-poppins",
     weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
