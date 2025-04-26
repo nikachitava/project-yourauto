9e77d1f1d29b30ea92d2de263a1167abce4c8aa3
nikachitava <chitava18.official@gmail.com>
2025-04-26 21:44:40 +0400

    allow all img url pattern

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
+    /* config options here */
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '**',
+          },
+          {
+            protocol: 'http',
+            hostname: '**',
+          },
+        ],
+      },
 };
 
 export default nextConfig;
-
-
