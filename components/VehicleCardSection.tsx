aa2c26049774f0471995ec58ba35a498cf8816e3
nikachitava <chitava18.official@gmail.com>
2025-04-28 14:12:35 +0400

    add filter on the main page

--- components/VehicleCardSection.tsx
+++ components/VehicleCardSection.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { fetchAllVehicles } from "@/lib/services/vehicleServices";
 import { IVehicle } from "@/types/IVehicle";
 import VehicleCard from "@/components/VehicleCard";
 import Loader from "./Loader";
+import { FilterMenuContext } from "@/context/FilterMenuProvider";
 
 const VehicleCardSection = () => {
     const [vehicles, setVehicles] = useState<IVehicle[]>([]);
     const [loading, setLoading] = useState(true);
+
+    const [filteredVehicles, setFilteredVehicles] = useState<IVehicle[]>([]);
 
     useEffect(() => {
         const getVehicles = async () => {
@@ -20,6 +23,56 @@ const VehicleCardSection = () => {
         getVehicles();
     }, []);
 
+    const { selectedBrand, selectedModel, year, price, selectedFuel, status } =
+        useContext(FilterMenuContext);
+
+    const filtereVehicles = () => {
+        let filtered = vehicles;
+
+        if (selectedBrand) {
+            filtered = filtered.filter(
+                (vehicle) => vehicle.brand_id === selectedBrand.id
+            );
+        }
+        if (selectedModel) {
+            filtered = filtered.filter(
+                (vehicle) => vehicle.model_id === selectedModel.id
+            );
+        }
+        if (year.max) {
+            filtered = filtered.filter((vehicle) => vehicle.year <= year.max);
+        }
+        if (year.min) {
+            filtered = filtered.filter((vehicle) => vehicle.year >= year.min);
+        }
+        if (price.max) {
+            filtered = filtered.filter((vehicle) => vehicle.price <= price.max);
+        }
+        if (price.min) {
+            filtered = filtered.filter((vehicle) => vehicle.price >= price.min);
+        }
+        if (selectedFuel) {
+            filtered = filtered.filter(
+                (vehicle) => vehicle.fuel_type === selectedFuel.name
+            );
+        }
+        if (status) {
+            filtered = filtered.filter((vehicle) => vehicle.status === status);
+        }
+        setFilteredVehicles(filtered);
+    };
+    useEffect(() => {
+        filtereVehicles();
+    }, [
+        selectedBrand,
+        selectedModel,
+        year,
+        price,
+        selectedFuel,
+        status,
+        vehicles,
+    ]);
+
     if (loading) return <Loader text="Loading vehicles..." />;
 
     return (
@@ -29,21 +82,27 @@ const VehicleCardSection = () => {
             </h1>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-                {vehicles.map((v) => (
-                    <VehicleCard
-                        key={v.id}
-                        id={v.id}
-                        city={v.city}
-                        country={v.country}
-                        cover_image={v.cover_image}
-                        title={v.title}
-                        year={v.year}
-                        price={v.price}
-                        mileage={v.mileage}
-                        fuel_type={v.fuel_type}
-                        transmission={v.transmission}
-                    />
-                ))}
+                {filteredVehicles.length > 0 ? (
+                    filteredVehicles.map((v) => (
+                        <VehicleCard
+                            key={v.id}
+                            id={v.id}
+                            city={v.city}
+                            country={v.country}
+                            cover_image={v.cover_image}
+                            title={v.title}
+                            year={v.year}
+                            price={v.price}
+                            mileage={v.mileage}
+                            fuel_type={v.fuel_type}
+                            transmission={v.transmission}
+                        />
+                    ))
+                ) : (
+                    <div className="col-span-1 sm:col-span-2 lg:col-span-4 text-center text-gray-500">
+                        No vehicles found.
+                    </div>
+                )}
             </div>
         </div>
     );
